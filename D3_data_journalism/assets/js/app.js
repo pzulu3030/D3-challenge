1e4fbc41534301f24ef451e1a057f19b7727f91e
pparashar68 <pparashar68@gmail.com>
2021-01-11 21:18:23 -0800

    Added boiler plate code for charting in d3

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,12 @@
-// @TODO: YOUR CODE HERE!
+const svgWidth = 960;
+const svgHeight = 500;
+
+const chartMargin = {
+  top: 30,
+  bottom: 30,
+  left: 30,
+  right: 30,
+};
+
+const chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+const chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
